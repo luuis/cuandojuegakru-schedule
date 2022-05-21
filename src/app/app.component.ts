164de16a532c65d5a723b94ee0eac5eaaefee5d4
Luis Enrique Suarez Rosas <luuis.e@outlook.com>
2022-05-21 04:43:14 -0500

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cuandojuegakru-schedule';
+  startDate: Date = new Date();
+  endDate: Date = new Date();
+  schedule: Schedule = {
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: []
+  };
+  weekDays: WeekDay[] = [
+    { name: 'Lunes', value: 'monday' },
+    { name: 'Martes', value: 'tuesday' },
+    { name: 'Miércoles', value: 'wednesday' },
+    { name: 'Jueves', value: 'thursday' },
+    { name: 'Viernes', value: 'friday' },
+    { name: 'Sábado', value: 'saturday' },
+    { name: 'Domingo', value: 'sunday' }
+  ];
+  tournaments: string[] = [
+    'FIFAe',
+    'Liga_Master_Flow',
+    'RLCS_2021-22',
+    'VALORANT_Challengers',
+    'VALORANT_Masters',
+    'VALORANT_Champions'
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  onStartDateChange(startDate: Date): void {
+    this.startDate = new Date(startDate);
+  }
+  onEndDateChange(endDate: Date): void {
+    this.endDate = new Date(endDate);
+  }
+
+  eventForm = this.formBuilder.group({
+    day: 'monday',
+    tournament: 'FIFAe',
+    opponent: '',
+    date: new Date()
+  });
+
+  onSubmit(): void {
+    const value = this.eventForm.value;
+    this.schedule[value.day].push({ tournament: value.tournament, opponent: value.opponent, date: new Date(value.date) });
+  }
+}
+
+type Schedule = {
+  [key: string]: Event[];
+  monday: Event[];
+  tuesday: Event[];
+  wednesday: Event[];
+  thursday: Event[];
+  friday: Event[];
+  saturday: Event[];
+  sunday: Event[];
+}
+
+type Event = {
+  tournament: string;
+  opponent: string;
+  date: Date;
+}
+
+type WeekDay = {
+  name: string;
+  value: string;
 }
